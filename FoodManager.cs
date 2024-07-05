bc8fb7bd37320205021c9339ca4c7ddae44e4381
datain01 <baksoeun01@gmail.com>
2024-07-05 14:28:27 +0900

    먹이주면 호감도 +5

--- FoodManager.cs
+++ FoodManager.cs
@@ -5,6 +5,8 @@ public class FoodManager : MonoBehaviour
 {
     public GameObject foodPrefab;
     public Transform foodSpawn;
+    public LikeSave likeSave; // LikeSave 스크립트를 참조
+    public LikeDisplay likeDisplay; // LikeDisplay 스크립트를 참조
 
     private GameObject currentFood;
 
@@ -30,5 +32,23 @@ public class FoodManager : MonoBehaviour
 
         // Food 재생성
         SpawnFood();
+
+        // Like 값 증가
+        IncreaseLike("CharacterOne");
+    }
+
+    private void IncreaseLike(string characterTag)
+    {
+        int currentLikes = PlayerPrefs.GetInt(characterTag + "_like", 0);
+        currentLikes += 5; // +5 증가
+        PlayerPrefs.SetInt(characterTag + "_like", currentLikes);
+        PlayerPrefs.Save();
+        Debug.Log(characterTag + " likes: " + currentLikes);
+
+        // LikeDisplay 업데이트
+        if (likeDisplay != null && likeDisplay.characterTag == characterTag)
+        {
+            likeDisplay.UpdateLikeDisplay();
+        }
     }
 }
--- FoodManagerSec.cs
+++ FoodManagerSec.cs
@@ -5,6 +5,8 @@ public class FoodManagerSec : MonoBehaviour
 {
     public GameObject foodSecPrefab;
     public Transform foodSpawnSec;
+    public LikeSave likeSave; // LikeSave 스크립트를 참조
+    public LikeDisplay likeDisplay; // LikeDisplay 스크립트를 참조
 
     private GameObject currentFood;
 
@@ -20,15 +22,33 @@ public class FoodManagerSec : MonoBehaviour
 
     public IEnumerator RespawnFoodAndChangeColor(SpriteRenderer characterRenderer)
     {
-        // CharacterOne의 색을 빨간색으로 변경
+        // CharacterSec의 색을 파란색으로 변경
         characterRenderer.color = Color.blue;
 
         yield return new WaitForSeconds(1f);
 
-        // CharacterOne의 색을 원래색으로 되돌림
+        // CharacterSec의 색을 시안색으로 되돌림
         characterRenderer.color = Color.cyan;
 
         // Food 재생성
         SpawnFood();
+
+        // Like 값 증가
+        IncreaseLike("CharacterSec");
+    }
+
+    private void IncreaseLike(string characterTag)
+    {
+        int currentLikes = PlayerPrefs.GetInt(characterTag + "_like", 0);
+        currentLikes += 5; // +5 증가
+        PlayerPrefs.SetInt(characterTag + "_like", currentLikes);
+        PlayerPrefs.Save();
+        Debug.Log(characterTag + " likes: " + currentLikes);
+
+        // LikeDisplay 업데이트
+        if (likeDisplay != null && likeDisplay.characterTag == characterTag)
+        {
+            likeDisplay.UpdateLikeDisplay();
+        }
     }
 }
